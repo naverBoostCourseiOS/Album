ce56b3ef3e42f7bf8dede20db1a3b7002c72d8c8
kangddong <rkdehddud96@naver.com>
2023-03-07 00:12:52 +0900

    Chore- 폴더 구분

---
+++ Rx/MyAlbum/MyAlbum/Presentaions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 강동영 on 2023/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
