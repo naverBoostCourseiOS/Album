fa7ebfd4a27220b0edd9322900733c2f4f21ae68
kangddong <rkdehddud96@naver.com>
2023-03-07 23:01:27 +0900

    [Refactor] - 찐 코드베이스로 해보기..! 스토리보드 -> 코드베이스 구조 변경

---
+++ Rx/MyAlbum/MyAlbum/Presentaions/AlbumGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumGridCell.swift
+//  MyAlbum
+//
+//  Created by 강동영 on 2023/03/07.
+//
+
+import UIKit
+
+class AlbumGridCell: UICollectionViewCell {
+    
+    static let identifer: String = String(describing: AlbumGridCell.self)
+}
--- Rx/MyAlbum/MyAlbum/Presentaions/ViewController.swift
+++ Rx/MyAlbum/MyAlbum/Presentaions/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero)
+        collectionView.register(AlbumGridCell.self, forCellWithReuseIdentifier: AlbumGridCell.identifer)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "앨범"
+        view.backgroundColor = .red
     }
 
 
+}
+
+// MARK: UICollectionViewDelegate, UICollectionViewDataSource Method
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumGridCell.identifer, for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
 
