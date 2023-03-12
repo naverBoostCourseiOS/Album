1e5816a757a48e7bcd6fe399fdf2957119ac13cf
doyeonjeong <debby_@kakao.com>
2023-03-12 14:55:23 +0900

    🎉 [Create]: Project init
    프로젝트 생성 및 코드 베이스로 구성하기 위한 프로젝트 설정

---
+++ doyeon/Album/Album/Sources/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Doyeon on 2023/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let testLabel = UILabel()
+        view.addSubview(testLabel)
+        testLabel.text = "test"
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+
+}
+
