088c2c2e02fbb9fd9694fc1137628926055303e5
kangddong <rkdehddud96@naver.com>
2023-03-27 15:35:48 +0900

    🎨[Design]: Base 브랜치를 위한 기초 UI 작업

--- Rx/MyAlbum/MyAlbum/Presentaions/AlbumGridCell.swift
+++ Rx/MyAlbum/MyAlbum/Presentaions/AlbumGridCell.swift
@@ -10,4 +10,48 @@ import UIKit
 class AlbumGridCell: UICollectionViewCell {
     
     static let identifer: String = String(describing: AlbumGridCell.self)
+    
+    public var imageView: UIImageView = UIImageView.init(frame: .zero)
+    public var albumTitleLabel: UILabel = UILabel.init(frame: .zero)
+    public var albumCountLabel: UILabel = UILabel.init(frame: .zero)
+    private let vStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        initLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initLayout() {
+        let views = [
+            imageView,
+            albumTitleLabel,
+            albumCountLabel,
+        ]
+        addSubview(vStackView)
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        views.forEach({
+            self.vStackView.addArrangedSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
+        
+        NSLayoutConstraint.activate([
+            vStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            vStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            vStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            vStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            imageView.widthAnchor.constraint(equalToConstant: contentView.frame.width),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+        ])
+    }
 }
--- Rx/MyAlbum/MyAlbum/Presentaions/ViewController.swift
+++ Rx/MyAlbum/MyAlbum/Presentaions/ViewController.swift
@@ -8,18 +8,21 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero)
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = CGSize(width: (UIScreen.main.bounds.width / 2) - 20, height: 220)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(AlbumGridCell.self, forCellWithReuseIdentifier: AlbumGridCell.identifer)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         
         return collectionView
     }()
     
-    var fetchRequest: PHFetchResult<PHAsset>!
+    var fetchResult: PHFetchResult<PHAsset>!
     let imageManager: PHCachingImageManager = PHCachingImageManager()
     
     func requestCollection() {
@@ -29,13 +32,56 @@ class ViewController: UIViewController {
         
         let fetchOptions = PHFetchOptions()
         fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        self.fetchRequest = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOptions)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
         navigationItem.title = "앨범"
         view.backgroundColor = .red
+        
+        // PHPhotoLibrary.authorizationStatus() Deprecated in future
+        let photoAutoriztionStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        
+        switch photoAutoriztionStatus {
+        case .authorized:
+            print("authorized")
+            requestCollection()
+        case .denied:
+            print("denied")
+        case .notDetermined:
+            print("notDetermined")
+            // PHPhotoLibrary.requestAuthorization(<#T##handler: (PHAuthorizationStatus) -> Void##(PHAuthorizationStatus) -> Void#>) Deprecated in future
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                switch status {
+                case .authorized:
+                    self.requestCollection()
+                case .denied:
+                    print("denied")
+                case .limited:
+                    print("limited")
+                    
+                default: break
+                }
+            }
+        case .restricted:
+            print("restricted")
+        case .limited:
+            print("limited")
+        @unknown default:
+            fatalError()
+        }
+        
+        collectionView.reloadData()
     }
 
 
@@ -44,15 +90,25 @@ class ViewController: UIViewController {
 // MARK: UICollectionViewDelegate, UICollectionViewDataSource Method
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return fetchResult?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumGridCell.identifer, for: indexPath)
+        guard let convertedCell = cell as? AlbumGridCell else { return cell }
         
-        return cell
-    }
-    
-    
-}
+        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+        
+        imageManager.requestImage(for: asset,
+                                  targetSize: CGSize(width: 30, height: 30),
+                                  contentMode: .aspectFill,
+                                  options: nil,
+                                  resultHandler: { image, _ in
 
+            convertedCell.imageView.image = UIImage(systemName: "photo")
+            convertedCell.albumTitleLabel.text = "dummyTitle"
+            convertedCell.albumCountLabel.text = "100"
+        })
+        return convertedCell
+    }
+}
